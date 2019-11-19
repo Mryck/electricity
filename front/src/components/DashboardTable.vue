a4fcb29c7d5939267b26b76b3263992018d36781
Mryck <emeric.bombenger@me.com>
2019-11-19 14:08:32 +0100

    Add table to display usefull values

--- front/src/components/DashboardTable.vue
+++ front/src/components/DashboardTable.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <table class="table is-striped">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Kwh heure pleine</th>
+          <th>Prix heure pleine</th>
+          <th>Kwh heure creuse</th>
+          <th>Prix heure creuse</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entry in data" :key="entry.date">
+          <td class="has-text-centered">{{entry.date}}</td>
+          <td class="has-text-centered">{{entry.hPleines}}</td>
+          <td class="has-text-centered"></td>
+          <td class="has-text-centered">{{entry.hCreuses}}</td>
+          <td class="has-text-centered"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
+  props: {
+    data: Array
+  },
   data() {
     return {};
   }
